e0faa13f1dc689837f78f83f2e7c603dd352f6a7
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-12 15:52:03 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = createItemsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onClickEvent);
+
+function onClickEvent(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  gallery.addEventListener("keydown", onEsc);
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+
+  function onEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
